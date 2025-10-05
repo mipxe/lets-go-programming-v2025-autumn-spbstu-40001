b28fae790e34eb8b438c043ac2bade970cc1bfe5
Mozhegova Maria <mariamozegova8726@gmail.com>
2025-10-05 03:10:56 +0300

    [TASK-2-1] Realization

--- maria.mozhegova/task-2-1/cmd/service/main.go
+++ maria.mozhegova/task-2-1/cmd/service/main.go
@@ -1,4 +1,52 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+	var departNum uint
+	_, err := fmt.Scan(&departNum)
+	if err != nil {
+		fmt.Println("Invalid input")
+		return
+	}
+	for range departNum {
+		var employNum, resultTemperature int
+		minTemperature, maxTemperature := 15, 30
+		_, err := fmt.Scan(&employNum)
+		if err != nil {
+			fmt.Println("Invalid input")
+			return
+		}
+		for range employNum {
+			var condition string
+			_, err := fmt.Scan(&condition)
+			if err != nil {
+				fmt.Println("Invalid input")
+				return
+			}
+			var currentTemperature int
+			_, err = fmt.Scan(&currentTemperature)
+			if err != nil {
+				fmt.Println("Invalid input")
+				return
+			}
+			switch condition {
+			case "<=":
+				maxTemperature = min(maxTemperature, currentTemperature)
+				resultTemperature = min(maxTemperature, resultTemperature)
+			case ">=":
+				minTemperature = max(minTemperature, currentTemperature)
+				resultTemperature = max(minTemperature, resultTemperature)
+			default:
+				fmt.Println("Invalid condition")
+				return
+			}
+			if (resultTemperature < minTemperature) || (resultTemperature > maxTemperature) {
+				resultTemperature = -1
+			}
+			fmt.Println(resultTemperature)
+		}
+	}
 }
