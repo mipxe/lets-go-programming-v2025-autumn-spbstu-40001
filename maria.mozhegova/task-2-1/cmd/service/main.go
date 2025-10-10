d7adaf86e10108efe9c52fb58a971a86227b08c8
Mozhegova Maria <mariamozegova8726@gmail.com>
2025-10-10 18:58:15 +0300

    [TASK-2-1] add struct

--- maria.mozhegova/task-2-1/cmd/service/main.go
+++ maria.mozhegova/task-2-1/cmd/service/main.go
@@ -4,65 +4,78 @@ import (
 	"fmt"
 )
 
+const (
+	minTemp = 15
+	maxTemp = 30
+)
+
+type Temperature struct {
+	min int
+	max int
+}
+
+func (temp *Temperature) printTempChange(cond string, currTemp int) {
+	switch cond {
+	case "<=":
+		temp.max = min(temp.max, currTemp)
+	case ">=":
+		temp.min = max(temp.min, currTemp)
+	default:
+		fmt.Println("Invalid condition")
+
+		return
+	}
+
+	if temp.min > temp.max {
+		fmt.Println(-1)
+	} else {
+		fmt.Println(temp.min)
+	}
+}
+
 func main() {
 	var departNum uint
 
 	_, err := fmt.Scan(&departNum)
 	if err != nil {
-		fmt.Println("Invalid input")
+		fmt.Println("Invalid input:", err)
 
 		return
 	}
 
 	for range departNum {
-		var employNum int
-
+		var (
+			employNum   int
+			temperature = Temperature{minTemp, maxTemp}
+		)
 		_, err := fmt.Scan(&employNum)
 		if err != nil {
-			fmt.Println("Invalid input")
+			fmt.Println("Invalid input:", err)
 
 			return
 		}
 
-		minTemperature := 15
-		maxTemperature := 30
-
 		for range employNum {
 			var (
-				condition          string
-				currentTemperature int
+				condition   string
+				currentTemp int
 			)
 
 			_, err := fmt.Scan(&condition)
 			if err != nil {
-				fmt.Println("Invalid input")
+				fmt.Println("Invalid input:", err)
 
 				return
 			}
 
-			_, err = fmt.Scan(&currentTemperature)
+			_, err = fmt.Scan(&currentTemp)
 			if err != nil {
-				fmt.Println("Invalid input")
+				fmt.Println("Invalid input:", err)
 
 				return
 			}
 
-			switch condition {
-			case "<=":
-				maxTemperature = min(maxTemperature, currentTemperature)
-			case ">=":
-				minTemperature = max(minTemperature, currentTemperature)
-			default:
-				fmt.Println("Invalid condition")
-
-				return
-			}
-
-			if minTemperature > maxTemperature {
-				fmt.Println(-1)
-			} else {
-				fmt.Println(minTemperature)
-			}
+			temperature.printTempChange(condition, currentTemp)
 		}
 	}
 }
