450255b9095909482bc38d77c9ab520f02780cae
Mozhegova Maria <mariamozegova8726@gmail.com>
2025-10-10 20:25:44 +0300

    [TASK-2-2] implementing the heap interface

--- maria.mozhegova/task-2-2/cmd/service/main.go
+++ maria.mozhegova/task-2-2/cmd/service/main.go
@@ -1,5 +1,41 @@
 package main
 
+type IntHeap []int
+
+func (h *IntHeap) Len() int {
+	return len(*h)
+}
+
+func (h *IntHeap) Less(i, j int) bool {
+	return (*h)[i] < (*h)[j]
+}
+
+func (h *IntHeap) Swap(i, j int) {
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
+}
+
+func (h *IntHeap) Push(x any) {
+	value, ok := x.(int)
+	if ok {
+		*h = append(*h, value)
+	} else {
+		panic("value is not an int")
+	}
+}
+
+func (h *IntHeap) Pop() any {
+	if h.Len() == 0 {
+		return nil
+	}
+
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+
+	return x
+}
+
 func main() {
 
 }
