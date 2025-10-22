11b7c14378736a2bef86f6e4e164457f2a500a46
Mozhegova Maria <mariamozegova8726@gmail.com>
2025-10-23 01:48:34 +0300

    [TASK-3] processing config

--- maria.mozhegova/task-3/cmd/service/main.go
+++ maria.mozhegova/task-3/cmd/service/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"flag"
+
+	"github.com/mipxe/task-3/internal/config"
+)
+
 func main() {
+	configPath := flag.String("config", "", "path to yaml file")
+	flag.Parse()
+
+	if *configPath == "" {
+		panic("config flag is required")
+	}
+
+	config, err := config.ReadConfig(*configPath)
+	if err != nil {
+		panic(err)
+	}
 
 }
